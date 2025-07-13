33a0f38f890c83dae3f2acd260ab3cb2ce667bf7
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-07-13 08:10:35 +0200

    🔧 Correcta estructura de argumentos en función peek()

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M:peek(job)
     local child = Command("mdcat")
-        :args({
+        :arg({
             "--columns",
             tostring(job.area.w),
             tostring(job.file.url),
